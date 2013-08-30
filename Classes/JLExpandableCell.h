807d96bd03133b2cbe2add91fa4adcb51d723f6d
David <agile_check_in@Davids-MacBook-Pro.local>
2013-08-29 22:58:08 -0400

    [#00] I'm an asshat.... add the damn properties

--- Classes/JLExpandableCell.h
+++ Classes/JLExpandableCell.h
@@ -1,5 +1,17 @@
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+@class JLExpandableCellRow;
 
 @interface JLExpandableCell : UIView
+@property(nonatomic, copy) UIColor *innerColor;
+@property(nonatomic, copy) UIColor *wrapperColor;
+@property(nonatomic) CGFloat corner_radius;
 
+- (id)initWithYPosition:(CGFloat)yPoint;
+
+- (JLExpandableCellRow *)newRow;
+
+- (void)insertView:(UIView *)childView;
+
+- (void)newDivision;
 @end
